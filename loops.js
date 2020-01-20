58bd7e627bfc21a8f559acc55ee2b3f98bfbf208
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-20 02:34:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,3 +18,14 @@ while (countdown > 0) {
 }
 
 function doWhileLoop()
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
